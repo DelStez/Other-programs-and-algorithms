3f3273114d469b43d9b451affd1777158c1dad58
RdrStels <50168261+RdrStels@users.noreply.github.com>
2020-11-29 01:37:27 +0500

    MT update

--- MT/FormMT.Designer.cs
+++ MT/FormMT.Designer.cs
@@ -32,8 +32,6 @@
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.LongOfTape = new System.Windows.Forms.ComboBox();
-            this.labelLong = new System.Windows.Forms.Label();
             this.labelAlpha = new System.Windows.Forms.Label();
             this.Alphabet = new System.Windows.Forms.TextBox();
             this.InputBox = new System.Windows.Forms.TextBox();
@@ -70,6 +68,7 @@
             this.tableOfInfo.RowHeadersVisible = false;
             this.tableOfInfo.Size = new System.Drawing.Size(636, 253);
             this.tableOfInfo.TabIndex = 0;
+            this.tableOfInfo.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.tableOfInfo_CellEndEdit);
             // 
             // Column1
             // 
@@ -98,34 +97,10 @@
             this.dataGridView2.Size = new System.Drawing.Size(740, 77);
             this.dataGridView2.TabIndex = 1;
             // 
-            // LongOfTape
-            // 
-            this.LongOfTape.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(199)))), ((int)(((byte)(193)))));
-            this.LongOfTape.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.LongOfTape.FormattingEnabled = true;
-            this.LongOfTape.Items.AddRange(new object[] {
-            "128",
-            "512",
-            "1024"});
-            this.LongOfTape.Location = new System.Drawing.Point(95, 9);
-            this.LongOfTape.Name = "LongOfTape";
-            this.LongOfTape.Size = new System.Drawing.Size(90, 21);
-            this.LongOfTape.TabIndex = 2;
-            this.LongOfTape.SelectedIndexChanged += new System.EventHandler(this.LongOfTape_SelectedIndexChanged);
-            // 
-            // labelLong
-            // 
-            this.labelLong.AutoSize = true;
-            this.labelLong.Location = new System.Drawing.Point(12, 12);
-            this.labelLong.Name = "labelLong";
-            this.labelLong.Size = new System.Drawing.Size(77, 13);
-            this.labelLong.TabIndex = 3;
-            this.labelLong.Text = "Длина ленты:";
-            // 
             // labelAlpha
             // 
             this.labelAlpha.AutoSize = true;
-            this.labelAlpha.Location = new System.Drawing.Point(198, 12);
+            this.labelAlpha.Location = new System.Drawing.Point(10, 12);
             this.labelAlpha.Name = "labelAlpha";
             this.labelAlpha.Size = new System.Drawing.Size(54, 13);
             this.labelAlpha.TabIndex = 4;
@@ -136,7 +111,7 @@
             this.Alphabet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(199)))), ((int)(((byte)(193)))));
             this.Alphabet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Alphabet.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(140)))), ((int)(((byte)(99)))));
-            this.Alphabet.Location = new System.Drawing.Point(258, 9);
+            this.Alphabet.Location = new System.Drawing.Point(70, 9);
             this.Alphabet.Name = "Alphabet";
             this.Alphabet.Size = new System.Drawing.Size(155, 20);
             this.Alphabet.TabIndex = 5;
@@ -146,7 +121,7 @@
             this.InputBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(199)))), ((int)(((byte)(193)))));
             this.InputBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.InputBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(140)))), ((int)(((byte)(99)))));
-            this.InputBox.Location = new System.Drawing.Point(515, 9);
+            this.InputBox.Location = new System.Drawing.Point(327, 9);
             this.InputBox.Name = "InputBox";
             this.InputBox.Size = new System.Drawing.Size(137, 20);
             this.InputBox.TabIndex = 6;
@@ -154,7 +129,7 @@
             // labelInput
             // 
             this.labelInput.AutoSize = true;
-            this.labelInput.Location = new System.Drawing.Point(419, 12);
+            this.labelInput.Location = new System.Drawing.Point(231, 12);
             this.labelInput.Name = "labelInput";
             this.labelInput.Size = new System.Drawing.Size(90, 13);
             this.labelInput.TabIndex = 7;
@@ -164,7 +139,7 @@
             // 
             this.getButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(112)))), ((int)(((byte)(86)))));
             this.getButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.getButton.Location = new System.Drawing.Point(658, 7);
+            this.getButton.Location = new System.Drawing.Point(470, 7);
             this.getButton.Name = "getButton";
             this.getButton.Size = new System.Drawing.Size(75, 23);
             this.getButton.TabIndex = 8;
@@ -177,7 +152,7 @@
             this.StartButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.StartButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(112)))), ((int)(((byte)(86)))));
             this.StartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.StartButton.Location = new System.Drawing.Point(496, 118);
+            this.StartButton.Location = new System.Drawing.Point(1, 118);
             this.StartButton.Name = "StartButton";
             this.StartButton.Size = new System.Drawing.Size(75, 23);
             this.StartButton.TabIndex = 9;
@@ -190,7 +165,7 @@
             this.clearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.clearButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(112)))), ((int)(((byte)(86)))));
             this.clearButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.clearButton.Location = new System.Drawing.Point(577, 119);
+            this.clearButton.Location = new System.Drawing.Point(82, 119);
             this.clearButton.Name = "clearButton";
             this.clearButton.Size = new System.Drawing.Size(75, 23);
             this.clearButton.TabIndex = 10;
@@ -237,8 +212,6 @@
             this.Controls.Add(this.InputBox);
             this.Controls.Add(this.Alphabet);
             this.Controls.Add(this.labelAlpha);
-            this.Controls.Add(this.labelLong);
-            this.Controls.Add(this.LongOfTape);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.tableOfInfo);
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(140)))), ((int)(((byte)(99)))));
@@ -258,8 +231,6 @@
 
         private System.Windows.Forms.DataGridView tableOfInfo;
         private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.ComboBox LongOfTape;
-        private System.Windows.Forms.Label labelLong;
         private System.Windows.Forms.Label labelAlpha;
         private System.Windows.Forms.TextBox Alphabet;
         private System.Windows.Forms.TextBox InputBox;
@@ -267,10 +238,10 @@
         private System.Windows.Forms.Button getButton;
         private System.Windows.Forms.Button StartButton;
         private System.Windows.Forms.Button clearButton;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.Button addButton;
         private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
     }
 }
 
--- MT/FormMT.cs
+++ MT/FormMT.cs
@@ -15,13 +15,13 @@ namespace MT
         public FormMT()
         {
             InitializeComponent();
-            LongOfTape.SelectedIndex = 0;
             tableOfInfo.Rows.Add();
             tableOfInfo.Rows[0].Cells[0].Value = "q0";
            
             
         }
-   
+
+        protected char[] moveSymbol = { '>', '<', '.' }; // right, left, stay
         string[,] Rules; 
         string alfabetstr;
         public void Machine()
@@ -39,6 +39,12 @@ namespace MT
                 {
                     if (tableOfInfo.Columns[i].HeaderText.ToString() == Val)
                     {
+                        var cellCommand = tableOfInfo.Rows[State].Cells[i].Value.ToString();
+                        string[] symbolAndNextState = cellCommand.Split(moveSymbol).ToArray();
+                        var moveVector = Array.Find(cellCommand.ToCharArray(), x => moveSymbol.Contains(x)).ToString();
+
+
+
                         List<string>temp = new List<string>(tableOfInfo.Rows[State].Cells[i].Value.ToString().Split(',').ToArray());
                         if(temp[0] != " ") dataGridView2.Rows[0].Cells[midlle].Value = temp[0];
                         if (temp[1] == "R") midlle++;
@@ -63,16 +69,16 @@ namespace MT
         void CreateTape() 
         {
             int i = 0;
-            while (i < Convert.ToInt32(LongOfTape.SelectedItem))
+            while (i <= 200)
             {
                 DataGridViewTextBoxColumn k = new DataGridViewTextBoxColumn();
-                k.HeaderText = (i - Convert.ToInt32(LongOfTape.SelectedItem) / 2).ToString();
+                k.HeaderText = (i - 200 / 2).ToString();
                 dataGridView2.Columns.Add(k);
                 dataGridView2.Rows[0].Cells[i].Value = "_";
                 i++;
             }
-            int x = dataGridView2.Columns[Convert.ToInt32(LongOfTape.SelectedItem) / 2].DisplayIndex;
-            dataGridView2.CurrentCell = dataGridView2[Convert.ToInt32(LongOfTape.SelectedItem) / 2, 0];
+            int x = dataGridView2.Columns[200/ 2].DisplayIndex;
+            dataGridView2.CurrentCell = dataGridView2[200 / 2, 0];
             dataGridView2.FirstDisplayedScrollingColumnIndex = x - dataGridView2.DisplayedColumnCount(true) / 2;
 
         }
@@ -84,14 +90,14 @@ namespace MT
         void CleanTapeAnInstructions()
         {
             int i = 0;
-            while (i < Convert.ToInt32(LongOfTape.SelectedItem))
+            while (i <= 200)
             {
                
                 dataGridView2.Rows[0].Cells[i].Value = "_";
                 i++;
             }
-            int x = dataGridView2.Columns[Convert.ToInt32(LongOfTape.SelectedItem) / 2].DisplayIndex;
-            dataGridView2.CurrentCell = dataGridView2[Convert.ToInt32(LongOfTape.SelectedItem) / 2, 0];
+            int x = dataGridView2.Columns[200 / 2].DisplayIndex;
+            dataGridView2.CurrentCell = dataGridView2[200 / 2, 0];
             dataGridView2.FirstDisplayedScrollingColumnIndex = x - dataGridView2.DisplayedColumnCount(true) / 2;
             while (1 < tableOfInfo.Columns.Count - 1) tableOfInfo.Columns.RemoveAt(1);
             while (1 < tableOfInfo.Rows.Count) tableOfInfo.Rows.RemoveAt(1);
@@ -110,9 +116,10 @@ namespace MT
             string StrForMachine = (InputBox.Text).ToString();
             for (int i = 0; i < StrForMachine.Length; i++)
             {
-                dataGridView2[(Convert.ToInt32(LongOfTape.SelectedItem) / 2) + i, 0].Value = StrForMachine[i];
+                dataGridView2[(200 / 2) + i, 0].Value = StrForMachine[i];
             }
         }
+
 
         private void startButton_Click(object sender, EventArgs e)
         {
@@ -124,7 +131,6 @@ namespace MT
             CleanTapeAnInstructions();
             Alphabet.Clear();
             InputBox.Clear();
-            LongOfTape.SelectedIndex = 0;
         }
 
         private void addButton_Click(object sender, EventArgs e)
@@ -139,10 +145,51 @@ namespace MT
             tableOfInfo.Rows.RemoveAt(tableOfInfo.Rows.Count - 1);
         }
 
-        private void LongOfTape_SelectedIndexChanged(object sender, EventArgs e)
+        private void tableOfInfo_CellEndEdit(object sender, DataGridViewCellEventArgs e)
         {
-            dataGridView2.Columns.Clear();
-            CreateTape();
+
+            try
+            {
+                var currentContent = tableOfInfo[e.ColumnIndex, e.RowIndex].Value.ToString();
+                Type type = e.GetType();
+                if (!currentContent.Contains("q"))
+                {
+                    string[] c = currentContent.Split(moveSymbol).ToArray();
+                    if (c.Length == 1)
+                    {
+                        MessageBox.Show("Не задана команда перехода");
+                        tableOfInfo[e.ColumnIndex, e.RowIndex].Value = null;
+                    }
+                    else
+                    {
+                        if (c[1] != "!" && c[1] != "" && Convert.ToInt32(c[1]) > tableOfInfo.Rows.Count - 1)
+                        {
+                            MessageBox.Show("Неверный номер состояния");
+                            tableOfInfo[e.ColumnIndex, e.RowIndex].Value = null;
+                        }
+                        else
+                        {
+                            string temp = "";
+                            if (c[0] == "")
+                                temp += e.ColumnIndex.ToString();
+                            else
+                            {
+                                temp += c[0].ToString();
+                            }
+                            temp += Array.Find(currentContent.ToCharArray(), x => moveSymbol.Contains(x)).ToString();
+                            if (c[1] == "")
+                                temp += e.RowIndex.ToString();
+                            else if (c[1] == "!" || c[1] != "" && Convert.ToInt32(c[1]) <= tableOfInfo.Rows.Count - 1)
+                            {
+                                temp += c[1].ToString();
+                            }
+
+                            tableOfInfo[e.ColumnIndex, e.RowIndex].Value = temp;
+                        }                        
+                    }
+                }
+            }
+            catch (System.NullReferenceException){ }
         }
     }
 }
